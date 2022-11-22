2d19b131c504dd9818b505c81e846a07afc5125d
Martin Schnabel <mb0@mb0.org>
2022-11-22 18:40:13 +0100

    qrypgx: add and register a postgres backend provider

---
+++ qrypgx/prov.go
@@ -0,0 +1,40 @@
+package qrypgx
+
+import (
+	"context"
+	"sync"
+
+	"github.com/jackc/pgx/v4/pgxpool"
+	"xelf.org/daql/dom"
+	"xelf.org/daql/qry"
+	"xelf.org/xelf/mod"
+)
+
+var Prov = qry.RegisterProvider(&provider{}, "postgres", "postgresql", "qrypgx")
+
+type provider struct{ pools sync.Map }
+
+func (p *provider) Provide(uri string, pro *dom.Project) (qry.Backend, error) {
+	loc := mod.ParseLoc(uri)
+	pguri := "postgres:" + loc.Path()
+	db, err := p.pool(pguri)
+	if err != nil {
+		return nil, err
+	}
+	return New(db, pro), nil
+}
+func (p *provider) pool(uri string) (*pgxpool.Pool, error) {
+	a, _ := p.pools.Load(uri)
+	if a == nil {
+		pool, err := pgxpool.Connect(context.Background(), uri)
+		if err != nil {
+			return nil, err
+		}
+		var loaded bool
+		a, loaded = p.pools.LoadOrStore(uri, pool)
+		if loaded {
+			pool.Close()
+		}
+	}
+	return a.(*pgxpool.Pool), nil
+}
